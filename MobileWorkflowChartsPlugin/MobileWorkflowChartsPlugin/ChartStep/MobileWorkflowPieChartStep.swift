919ab503e21a6e00a03a9cf4594b64655571e134
Jonathan Flintham <jonathan@futureworkshops.com>
2020-10-15 15:05:22 +0100

    Taking pie chart data from app.json

--- MobileWorkflowChartsPlugin/MobileWorkflowChartsPlugin/ChartStep/MobileWorkflowPieChartStep.swift
+++ MobileWorkflowChartsPlugin/MobileWorkflowChartsPlugin/ChartStep/MobileWorkflowPieChartStep.swift
@@ -9,16 +9,61 @@ import Foundation
 import MobileWorkflowCore
 import ResearchKit
 
-public class MobileWorkflowPieChartStep: ORKStep, BuildableStep {
-    public static var stepClassName: String {
-        return String(describing: self)
+enum L10n {
+    enum PieChart {
+        static let descriptionLabel = "Quantities"
+        static let legendLabel = "Types"
+    }
+}
+
+public enum MobileWorkflowChartStepType: String, MobileWorkflowStepType {
+    
+    case pieChart = "chartsPieChart"
+    
+    public var typeName: String {
+        return self.rawValue
     }
     
-    public static func build(data: StepData, networkManager: NetworkManager, imageLoader: ImageLoader, localizationManager: Localization) throws -> ORKStep {
-        return MobileWorkflowPieChartStep(identifier: data.identifier)
+    public var stepClass: MobileWorkflowStep.Type {
+        return MobileWorkflowPieChartStep.self
+    }
+}
+
+struct PieChartItem {
+    let label: String
+    let value: Double
+}
+
+public class MobileWorkflowPieChartStep: ORKStep {
+    
+    let items: [PieChartItem]
+    
+    init(identifier: String, items: [PieChartItem]) {
+        self.items = items
+        super.init(identifier: identifier)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     public override func stepViewControllerClass() -> AnyClass {
         return MobileWorkflowPieChartStepViewController.self
+    }
+}
+
+extension MobileWorkflowPieChartStep: MobileWorkflowStep {
+    
+    public static func build(data: StepData, networkManager: NetworkManager, imageLoader: ImageLoader, localizationManager: Localization) throws -> ORKStep {
+        
+        let itemContent = data.content["items"] as? [[String: Any]] ?? []
+        let items: [PieChartItem] = itemContent.map {
+            guard let label = $0["label"] as? String, let stringValue = $0["value"] as? String, let value = Double(stringValue) else {
+                fatalError()
+            }
+            return PieChartItem(label: label, value: value)
+        }
+        
+        return MobileWorkflowPieChartStep(identifier: data.identifier, items: items)
     }
 }
